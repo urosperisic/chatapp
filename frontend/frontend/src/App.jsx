74db8b305ce711ce106245741792667ca271aa9e
urosperisic <uros2907@gmail.com>
2025-10-25 02:00:47 +0200

    add initial styles

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,9 +1,13 @@
 function App() {
   return (
-    <>
-      <h1>MLADEN JE KRALJ</h1>
-      <h2>a mozda i car</h2>;
-    </>
+    <main>
+      <h1 className="success">MLADEN IS THE KING</h1>
+      <h2 className="error">or maybe the emperor</h2>
+      <p>
+        Check out <a href="#">this link</a> to see the contrast.
+      </p>
+      <button>Terminal button</button>
+    </main>
   );
 }
 
